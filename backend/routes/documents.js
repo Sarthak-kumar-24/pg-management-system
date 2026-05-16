2414fdc35ab955edb90908e5273f67ce3591473b
Sarthak Kumar <131984558+Sarthak-kumar-24@users.noreply.github.com>
2026-05-17 00:44:23 +0530

    Update documents.js

--- backend/routes/documents.js
+++ backend/routes/documents.js
@@ -7,7 +7,7 @@ router.get("/", auth, ctrl.list);
 //router.post("/", auth, ctrl.create);
 router.get("/:id", auth, ctrl.get);
 router.delete("/:id", auth, ctrl.remove);
-router.post("/", auth, ctrl.uploadMiddleware, ctrl.uploadDocument);
-router.post("/", ctrl.uploadMiddleware, ctrl.create);
+router.post("/", auth, ctrl.uploadMiddleware, ctrl.create);
+
 
 module.exports = router;
