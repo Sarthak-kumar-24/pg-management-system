c4ed2e8d59812962fa21b5a24518f014275f0844
Sarthak Kumar <131984558+Sarthak-kumar-24@users.noreply.github.com>
2026-05-17 00:39:25 +0530

    Update documents.js

--- backend/routes/documents.js
+++ backend/routes/documents.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const ctrl = require("../controllers/documentController");
 const auth = require("../middleware/auth");
 
+
 router.get("/", auth, ctrl.list);
 //router.post("/", auth, ctrl.create);
 router.get("/:id", auth, ctrl.get);
 router.delete("/:id", auth, ctrl.remove);
 router.post("/", auth, ctrl.uploadMiddleware, ctrl.uploadDocument);
+router.post("/", ctrl.uploadMiddleware, ctrl.create);
 
 module.exports = router;
